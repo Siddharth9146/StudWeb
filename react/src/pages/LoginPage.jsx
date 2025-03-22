105bcb4f24742461bcaa2076a0ec35db5127bd25
devdeep <udevdeep@gmail.com>
2025-03-23 01:36:26 +0530

    college list added, added aditional Dashboard when login, nothing, Student login api connect

--- react/src/pages/LoginPage.jsx
+++ react/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = ({ setIsAuthenticated }) => {
 
         try {
             const response = await fetch("http://127.0.0.1:8000/StudLogin", {
-                method: "POST",
+                method: "GET",
                 headers: {
                     "Content-Type": "application/json"
                 },
